b292fc369ebd8b09b4a260a051b4adfa32ed697b
Afutrus <afutrusvy@gmail.com>
2026-03-15 03:34:28 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,13 +1,18 @@
+import { kv } from '@vercel/kv';
+
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
-  
   const data = req.body;
+  
   if (data.status === 'completed') {
+    // 1. Simpan status lunas ke database Vercel KV selama 1 jam
+    await kv.set(`status:${data.order_id}`, 'lunas', { ex: 3600 });
+
+    // 2. Kirim Notif Telegram
     const BOT_TOKEN = "8597331224:AAFnZ8fuiYeyUKVlypItH1Gutz23PCOMT6Y";
     const CHAT_ID = "6604182176";
-    const pesan = `✅ *PEMBAYARAN QRIS PAKASIR SUKSES*%0A💰 Nominal: Rp ${data.amount.toLocaleString()}%0A🆔 Order ID: ${data.order_id}%0A👤 Player ID: ${data.reference || 'N/A'}`;
-    
+    const pesan = `✅ *TOPUP SUKSES*%0A🆔 INV: ${data.order_id}%0A💰 Nominal: Rp ${data.amount}`;
     await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${pesan}&parse_mode=Markdown`);
   }
+  
   res.status(200).send('OK');
 }
